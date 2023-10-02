9bd769763350287a4e1d82a3ab7c9b6060d6c047
Qridony <dombibalazs666@gmail.com>
2023-10-02 11:43:01 +0200

    Fájlok létrehozva

---
+++ Injectest/ExampleSingletonService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Injectest
+{
+    internal class ExampleSingletonService :IExampleSingletonService
+    {
+        Guid IReportServiceLifetime.Id { get; } = Guid.NewGuid();
+    }
+}
---
+++ Injectest/IExampleTransientService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Injectest
+{
+    public interface IExampleTransientService :IReportServiceLifetime
+    {
+        ServiceLifetime IReportServiceLifetime.Lifetime => ServiceLifetime.Transient;
+    }
+}
